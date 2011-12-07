7c841ccfb7a17788d6f8a5e8e9774407d98dd719
Henke Hedelund <henke.hedelund@gmail.com>
2011-12-07 20:00:33 +0100

    added license texts

--- freeztile.c
+++ freeztile.c
@@ -1,3 +1,31 @@
+/**
+ * libfreeztile
+ *
+ * Copyright (C) 2011 Henrik Hedelund (henke.hedelund@gmail.com)
+ *
+ * This file is part of libfreeztile.
+ *
+ * libfreeztile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libfreeztile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libfreeztile. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category libfreeztile
+ * @package libfreeztile
+ * @author Henrik Hedelund <henke.hedelund@gmail.com>
+ * @copyright 2012 Henrik Hedelund (henke.hedelund@gmail.com)
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPL
+ * @link freeztile.org
+ */
+
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
